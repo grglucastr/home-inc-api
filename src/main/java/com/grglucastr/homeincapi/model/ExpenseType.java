a7c0f80b6cf2fe76302ef91e2a202a7c0345802b
George Bentes <george.bentes@gmail.com>
2019-12-05 20:08:29 -0300

    Add '@JsonIgnore' to avoid duplication on Json when doing Lazy Fetch on expense type

--- src/main/java/com/grglucastr/homeincapi/model/ExpenseType.java
+++ src/main/java/com/grglucastr/homeincapi/model/ExpenseType.java
@@ -1,5 +1,6 @@
 package com.grglucastr.homeincapi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
@@ -29,5 +30,6 @@ public class ExpenseType implements Serializable {
     private ExpenseTypeCategory category;
 
     @OneToMany(mappedBy = "expenseType")
+    @JsonIgnore
     private Set<Expense> expenses;
 }
