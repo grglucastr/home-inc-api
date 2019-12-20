b1e217a2146d2c241458b186d9100f7a51cb1723
George Bentes <george.bentes@gmail.com>
2019-12-20 19:48:55 -0300

    Add JaCoCo for tests coverage report

--- src/test/java/com/grglucastr/homeincapi/HomeIncApiApplicationTests.java
+++ src/test/java/com/grglucastr/homeincapi/HomeIncApiApplicationTests.java
@@ -1,13 +1,18 @@
 package com.grglucastr.homeincapi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
-@SpringBootTest
+import static org.junit.Assert.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
 class HomeIncApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void testPass(){
+        assertTrue(true);
+    }
+
 
 }
