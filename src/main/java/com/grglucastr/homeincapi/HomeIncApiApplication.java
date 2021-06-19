e950865d1289e7fd8624e3cd0144de02a9bdf630
George Bentes <george.bentes@gmail.com>
2021-06-19 18:54:42 -0300

    Add JPA Audit and remove old Swagger validation

--- src/main/java/com/grglucastr/homeincapi/HomeIncApiApplication.java
+++ src/main/java/com/grglucastr/homeincapi/HomeIncApiApplication.java
@@ -2,28 +2,14 @@ package com.grglucastr.homeincapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableSwagger2
+@EnableJpaAuditing
 public class HomeIncApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeIncApiApplication.class, args);
-	}
-
-	//TODO: Add swagger configuration
-	public Docket api(){
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.grglucastr.homeincapi.controller"))
-				.paths(PathSelectors.any())
-				.build();
 	}
 
 }
