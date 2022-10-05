3c83901ab933bf192645ed2c1db83e3e76de9e51
= <george.bentes@gmail.com>
2022-10-04 22:10:30 -0300

    Set basic structure

---
+++ src/main/java/com/grglucastr/homeincapi/HomeIncApiApplication.java
@@ -0,0 +1,13 @@
+package com.grglucastr.homeincapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeIncApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeIncApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grglucastr/homeincapi/HomeIncApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grglucastr.homeincapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeIncApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
