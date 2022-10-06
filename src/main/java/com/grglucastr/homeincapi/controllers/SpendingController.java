bc40f8308ad9f188df5505dc7cd49023f8df7e14
= <george.bentes@gmail.com>
2022-10-05 22:53:29 -0300

    Add structure to work with TDD

--- src/main/java/com/grglucastr/homeincapi/controllers/SpendingController.java
+++ src/main/java/com/grglucastr/homeincapi/controllers/SpendingController.java
@@ -3,11 +3,13 @@ package com.grglucastr.homeincapi.controllers;
 import com.grglucastr.homeincapi.api.SpendingsApi;
 import com.grglucastr.homeincapi.models.BasicResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController("SpendingController")
 public class SpendingController implements SpendingsApi {
 
     @Override
     public ResponseEntity<BasicResponse> getSpendings() {
-        return SpendingsApi.super.getSpendings();
+        return ResponseEntity.ok(new BasicResponse());
     }
 }
