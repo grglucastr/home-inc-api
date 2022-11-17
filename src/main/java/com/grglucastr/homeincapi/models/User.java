a951b27c5fcf2e7a2450133faa23218e863cb97d
= <george.bentes@gmail.com>
2022-11-16 22:39:48 -0300

    Add User model class, rename User class OAS 3 to UserResponse

---
+++ src/main/java/com/grglucastr/homeincapi/models/User.java
@@ -0,0 +1,53 @@
+package com.grglucastr.homeincapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends BaseModel {
+
+    private String name;
+    private String email;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "se_users")
+    @SequenceGenerator(name = "se_users", sequenceName = "se_users")
+    @Override
+    public Long getId() {
+        return super.getId();
+    }
+
+    @Column(name = "active")
+    @Override
+    public Boolean getActive() {
+        return super.getActive();
+    }
+
+    @Column(name = "insert_date_time")
+    @Override
+    public LocalDateTime getInsertDateTime() {
+        return super.getInsertDateTime();
+    }
+
+    @Column(name = "update_date_time")
+    @Override
+    public LocalDateTime getUpdateDateTime() {
+        return super.getUpdateDateTime();
+    }
+}
